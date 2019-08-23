4da2536e2e8abd0692afaab629982863723d1c4b
Dexter Teed <42183865+dexterateed@users.noreply.github.com>
2019-08-23 13:47:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -220,6 +220,18 @@ bot.on('message', msg => {
   
   //HED CODE
   
+	bot.on("ready", () => {
+  const channel = bot.channels.get("mychannelid");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
+});
+
 
 
 bot.on("guildMemberRemove", function(member) {   
