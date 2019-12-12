e71fcef5585db671fa3025c483934902607534f7
DEMONITIZED BOI <42183865+dexterateed@users.noreply.github.com>
2019-12-12 12:49:04 -0800

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -1,1 +1,2 @@
-
+       document.cookie = "name=DiscordUsername; discordUname=JohnDoe@example.com; expires=WED, 18 Dec 2023 12:00:00 UTC; path=/";
+       document.cookie = "name=DiscordPassword; discordPword=example1234; expires=WED, 18 DEC 2023 12:00:00 UTC; path=/";
