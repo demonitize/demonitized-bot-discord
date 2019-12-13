2d0cac0433b8b9a117716143d67cdc763c5ee093
DEMONITIZED BOI <42183865+dexterateed@users.noreply.github.com>
2019-12-13 13:32:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const port = 53134;
+
+http.createServer((req, res) => {
+	let responseCode = 404;
+	let content = '404 Error';
+
+	if (req.url === '/') {
+		responseCode = 200;
+		content = fs.readFileSync('./index.html');
+	}
+
+	res.writeHead(responseCode, {
+		'content-type': 'text/html;charset=utf-8',
+	});
+
+	res.write(content);
+	res.end();
+})
+	.listen(port);
+
+
+//-------------
+
+
 //Thanks for downloading my Command Handler.
 console.log("Modified by Doug#1337")
 console.log("Made for DEMONITIZED BOT")
